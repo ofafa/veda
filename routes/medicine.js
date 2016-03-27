0e7badc4c77e05183aaeb43c85da56ec70f3c6f1
ofafa <chunghsianghsueh@gmail.com>
2016-03-26 22:41:36 -0700

    1. Finished the update function 2. Add simple UI for desks 3. Updated the data with simplified index 4. Adjust the display of search results

--- routes/medicine.js
+++ routes/medicine.js
@@ -43,13 +43,40 @@ router.get('/edit/:name', function(req, res, next){
 
 router.post('/edit/:name', function(req, res, next){
     //todo: update the data in database
-    Medicine.update({name:req.params.name}, function(err, medicine){
+    var conditions = {
+        name: req.params.name,
+        }, update = {$set:{
+        name: req.body['name'],
+        price: req.body['price'],
+        position: req.body['position'],
+        row: req.body['row'],
+        col: req.body['col'],
+        index: req.body['index'],
+        info: req.body['info']}},
+        options = {multi:false};
+
+    Medicine.update(conditions, update, options, function(err, medicine){
         if(!medicine){
             console.log('no this medicine ' + req.params.name + ' found');
             return res.redirect('/');
         }
+        if(err){
+            return callback(err);
+        }
+        console.log('medicine' + req.param.name + 'updated');
+        return res.redirect('/medicine');
+    });
+});
 
-    })
-})
+router.get('/medinfo/:name', function(req, res){
+    Medicine.findOne({name: req.params.name}, function(err, medicine){
+        if(err) {
+            console.log(err);
+            return callback(err);
+        }
+        res.render('medinfo', {user:'dev', medicine: medicine});
+    });
+
+});
 
 module.exports = router;
