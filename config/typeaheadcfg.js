1c00b2802cb9f1a143cab73032d045519f38762f
ofafa <chunghsianghsueh@gmail.com>
2016-12-29 01:02:10 +0800

    [1] Add typeahead data and config

---
+++ config/typeaheadcfg.js
@@ -0,0 +1,25 @@
+/**
+ * Created by s955281 on 08/12/2016.
+ */
+var Medicine = require('../model/medicine');
+var fs = require('fs');
+var medicines = [];
+
+
+
+var typeaheadcfg = function(){
+
+        Medicine.find({}, 'name -_id', function(err, medicineNames) {
+            if (err) console.log(err);
+            medicineNames.forEach(function (medicine) {
+                medicines.push('"' + medicine['name'] + '"');
+            });
+            //console.log('write meddata');
+            //console.log(medicines);
+            fs.writeFile('./typedata.json', '[' + medicines.toString() + ']', function(err){
+                if(err) console.log(err);
+            })
+        });
+};
+
+module.exports = typeaheadcfg;
