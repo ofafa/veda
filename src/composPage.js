cc9456a780ed59b7adae5fcea64f27179607ea1d
ofafa <chunghsianghsueh@gmail.com>
2017-03-19 17:15:32 +0800

    1.Add composPage and index.html

---
+++ src/composPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Composition from "../components/composition/composition.js";
+
+const app = document.getElementById('main');
+ReactDom.render(<Composition/>, app);
