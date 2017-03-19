a487928e7a2df8b59aaf4dc7ab064a95eb14d396
ofafa <chunghsianghsueh@gmail.com>
2017-03-19 17:06:33 +0800

    run with preinstall

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: path.join('./', 'src','composPage.js'),
+    entry: path.join(__dirname, 'src','composPage.js'),
     output: {
-        path: path.join('./', 'public', 'js'),
+        path: path.join(__dirname, 'public', 'js'),
         filename: 'bundle.js'
     },
     module:{
